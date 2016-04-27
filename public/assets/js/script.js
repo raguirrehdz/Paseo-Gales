1c4ebb03961bd02bf1eedd822e3258cf1bec16d8
raguirrehdz <rebekaaguirre@yahoo.com>
2016-04-27 19:12:20 -0400

    typo fix

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,4 @@
-/* Nav Bar Active Link Change */
+// Nav Bar Active Link Change
 
 $(document).ready(function () {
     $('.nav li').click(function(e) {
@@ -12,37 +12,3 @@ $(document).ready(function () {
         //e.preventDefault();
     });
 });
-
-/* Log In */
-/*
-$(document).ready(function check(login) /*function to check userid & password*/ {
-    /*the following code checkes whether the entered userid and password are matching*/
-/*
-    if (form.userid.value == "residentepg" && form.pswrd.value == "paseogales123") {
-      window.open('dropDownMenu') /*opens the target page while Id & password matches*/
-/*    } else {
-      alert("Error Password or Username") /*displays error message*/
-/*    }
-  });
-*/
-/*
-  var header = $('header');
-  var range = 200;
-
-  $(window).on('scroll', function () {
-
-      var scrollTop = $(this).scrollTop();
-      var offset = header.offset().top;
-      var height = header.outerHeight();
-      offset = offset + height / 2;
-      var calc = 1 - (scrollTop - offset + range) / range;
-
-      header.css({ 'opacity': calc });
-
-      if ( calc > '1' ) {
-        header.css({ 'opacity': 1 });
-      } else if ( calc < '0' ) {
-        header.css({ 'opacity': 0 });
-      }
-  });
-8?
